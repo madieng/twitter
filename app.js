61cc7283c0b49427b30fe79095d221f2a68b5816
Madieng <madieng.mbeguere@external.grdf.fr>
2019-11-19 15:02:41 +0100

    Twitter NodeJS

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+const port = process.env.PORT || 3003;
+const index = require('./routes');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(morgan('short'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(index);
+
+app.listen(port);
